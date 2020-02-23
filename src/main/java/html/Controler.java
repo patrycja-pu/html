72250561fed880f40d15ad90882a8ef0c4149645
stud-b00706 <stud-b00706@EDU-WSB.local>
2020-02-23 10:44:06 +0100

    html

---
+++ src/main/java/html/Controler.java
@@ -0,0 +1,14 @@
+package html;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controler {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+}
